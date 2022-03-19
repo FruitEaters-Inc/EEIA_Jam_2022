bc95d58418e463ddd88dae7c60f4e2c66de1aa86
Daniel Wieczorek <daniel.pazur@gmail.com>
2022-03-19 18:18:44 +0100

    Added navigate_to() prototype, add_target() and cleaned up movement

--- Drone.py
+++ Drone.py
@@ -8,7 +8,20 @@ class Drone:
         self.current_pos = self.current_tile.pos
         self.range = drone_range
         self.movePath = []
+        self.targets = []
         self.image = Image(source='drone.zip', pos=self.current_pos, nocache=True)
+
+    def add_target(self, target: Tile):
+        self.targets.append(target)
+
+    def navigate_to(self, target: Tile):
+        # find_path()
+        # movePath.append(path)
+        # movePath.append(path[::-1])
+            # Of course there'd have to be a delay between each move
+        # while len(self.movePath) != 0:
+        #     self.pop_move()
+        pass
 
     def pop_move(self):
         if len(self.movePath) == 0:
@@ -21,13 +34,12 @@ class Drone:
             return
         self.current_tile = target
         self.current_pos = target.pos
+        self.image.pos = target.pos
         target.get_interaction()
 
     def move_direction(self, direction: str) -> None:
         if direction in self.current_tile.neighbours.keys():
             self.move_to(self.current_tile.neighbours[direction])
-            self.current_pos = self.current_tile.pos
-            self.image.pos = self.current_tile.pos
 
     def draw_drone(self, layout):
         layout.add_widget(self.image)
