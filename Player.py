e91b0ba31e2c9002fd45402f2bfe362625f90d1d
Daniel Wieczorek <daniel.pazur@gmail.com>
2022-03-20 01:58:41 +0100

    Added Player drone handling

--- Player.py
+++ Player.py
@@ -1,7 +1,13 @@
+import Drone
+from Tile import Tile
+
+
 class Player:
-    def __init__(self):
+    def __init__(self, HQ_tile: Tile):
+        self.HQ_tile = HQ_tile
         self.wealth = 500
         self.drone_count = 0
+        self.drones = []
 
     def increaseWealth(self, count: int) -> None:
         self.wealth += count
@@ -10,11 +16,13 @@ class Player:
         self.wealth -= count
 
     def addDrone(self) -> None:
+        self.drones.append(Drone.Drone(self.HQ_tile, 10))
         self.drone_count += 1
 
     def removeDrone(self) -> None:
+        self.drones.pop()
         self.drone_count -= 1
 
     def purchaseDrone(self) -> None:
-        self.reduceWealth(500)
+        self.reduceWealth(250)
         self.addDrone()
