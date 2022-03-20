7098f293f3b2f93827e6bdfa01fa9cdd43945f50
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-03-20 02:07:52 +0100

    Added A* pathfinding

--- pathfinding.py
+++ pathfinding.py
@@ -1,4 +1,4 @@
-def dfs(start, end):
+def sdfs(start, end):
     road = list()
     visited, queue = set(), [start]
     while len(queue) != 0:
@@ -11,3 +11,50 @@ def dfs(start, end):
             for n in point.neighbours.values():
                 queue.append(n)
     return road
+
+def dfs(start, end):
+    open_list = set([start])
+    closed_list = set()
+
+    g = {}
+    g[start]  = 0
+
+    pairs = {}
+    pairs[start] = start
+
+    while len(open_list) > 0:
+        n = None
+
+        for v in open_list:
+            if n == None or g[v] < g[n]:
+                n = v
+
+        if n == None:
+            return None
+
+        if n == end:
+            reconst_path = []
+
+            while pairs[n] != n:
+                reconst_path.append(n)
+                n = pairs[n]
+
+            reconst_path.append(start)
+            reconst_path.reverse()
+            return reconst_path
+        
+        for m in n.neighbours.values():
+            if m not in open_list and m not in closed_list:
+                open_list.add(m)
+                pairs[m] = n
+                g[m] = g[n] + 1
+            else:
+                if g[m] > g[n] + 1:
+                    g[m] = g[n] + 1
+                    pairs[m] = n
+
+                    if m in closed_list:
+                        closed_list.remove(m)
+                        open_list.add(m)
+        open_list.remove(n)
+        closed_list.add(n)
